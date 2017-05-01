b16922063bd16683b05ddc62084a4f4aef283493
hectorj <hector.jusforgues@gmail.com>
2017-05-01 22:06:29 +0200

    Don't suppress logs from tests

--- test.sh
+++ test.sh
@@ -13,9 +13,9 @@ TEST_PKGS=$(find . -name '*_test.go' -exec dirname '{}' ';' | grep -v './vendor'
 echo "== Building tests..."
 go test -i -race $TEST_PKGS
 echo "== Starting tests..."
-gocov test $TEST_PKGS -v -race -coverpkg $(join "," $PACKAGES) 1> /dev/null 2> >(grep -v "warning: no packages being tested depend on" >&2)
+gocov test $TEST_PKGS -v -race -coverpkg $(join "," $PACKAGES) 1> >(grep -v "{\"Packages"  >&1) 2> >(grep -v "warning: no packages being tested depend on" >&2)
 echo "== Building strict tests..."
 go test -i -tags=strict -race $TEST_PKGS
 echo "== Starting strict tests..."
-gocov test $TEST_PKGS -v -tags=strict -race -coverpkg $(join "," $PACKAGES) 1> /dev/null 2> >(grep -v "warning: no packages being tested depend on" >&2)
+gocov test $TEST_PKGS -v -tags=strict -race -coverpkg $(join "," $PACKAGES) 1> >(grep -v "{\"Packages"  >&1) 2> >(grep -v "warning: no packages being tested depend on" >&2)
 echo "== Finished tests."
